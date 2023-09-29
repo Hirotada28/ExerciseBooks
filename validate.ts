620021bb67903ab3a6b06fcf9fc990495d07c9c2
nichicom 高野碩帝 <0687@corp.nck.co.jp>
2023-09-29 14:56:31 +0900

    修正

--- validate.ts
+++ validate.ts
@@ -1,4 +1,6 @@
 export const validateIsCorrect = <T>(actual: T, expect: T, id: string): void => {
+    console.log('あなたの解答');
+    console.log(actual);
   if (JSON.stringify(actual) === JSON.stringify(expect)) console.log(`${id}: 正解！`);
   else console.log(`${id}: 不正解...`);
 }
